37b188f0255169248d2459ace4c7609417d60cc8
Nawabkalra <nawabkalra@gmail.com>
2024-06-27 03:57:44 +0000

    Changes in 2nd

--- QuesString1.c
+++ QuesString1.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-int find(char *str,char c, char *sub) {
-	int a = strchr(str,c)?strchr(str,c)-str:-1;
-	int b=strstr(str,sub)?strstr(str,sub)-str:-1;
-
-	return a+b;
+int findCharacterIndex(char *str, char c) {
+    char *found = strchr(str, c);
+    if (found != NULL) {
+        return found - str;
+    } else {
+        return -1;
+	}
+}
+int findSubstringIndex(char *str, char *substring) {
+    char *found = strstr(str, substring);
+    if (found != NULL) {
+        return found - str;
+    } else {
+        return -1; 
+    }
 }
 
-int main(){
-	char str[100];
-	fgets(str,100,stdin);
-	char c;
-	scanf("%c",&c);
-	char sub[20];
-	scanf("%s",sub);
-	int a = find(str,c,sub);
-	printf("%d",a);
-	return 0;
+int main() {
+    char str[100], sub[20], c;
+    
+    // Input string
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Remove newline character from fgets
+
+    // Character
+    scanf(" %c", &c);
+
+    // Substring
+    scanf("%s", sub);
+
+    // Find indices
+    int characterIndex = findCharacterIndex(str, c);
+    int substringIndex = findSubstringIndex(str, sub);
+
+    // Print results
+    printf("%d\n", characterIndex);
+    printf("%d\n", substringIndex);
+
+    return 0;
 }
+
